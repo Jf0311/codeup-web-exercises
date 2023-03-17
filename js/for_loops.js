ca4ca78c7d0b364056aa5459739d36fd58cb2702
Joseph Fuentes <josephkfuentes@gmail.com>
2023-03-17 16:27:54 -0400

    iterating arrays

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,6 +1,30 @@
 function showMultiplicationTable(number){
-    for (var i = 1; i <= 12; i++) {
+    for (var i = 1; i <= 10; i++) {
         console.log(number + " * " + i + " = ", i * number );
     }
 }
-console.log(showMultiplicationTable(8));
+showMultiplicationTable(8);
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+         for (var i = 1; i <= 10; i++) {
+             var m = getRandomInt(200, 20);
+             if (m % 2 === 0) {
+                 console.log(i + m + " is even")
+             } else {
+                 console.log(i + m + " is odd")
+             }
+         }
+         for (var x =1; x <= 10; x++){
+             var double = x * 11
+             console.log(double)
+         }
+
+
+
+
+
