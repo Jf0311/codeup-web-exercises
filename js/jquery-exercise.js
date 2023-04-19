3fa01839201a29fae41430c64eae433c5cda4691
Joseph Fuentes <josephkfuentes@gmail.com>
2023-04-19 16:39:30 -0400

    Finished jquery exercise for mouse events

--- js/jquery-exercise.js
+++ js/jquery-exercise.js
@@ -1,5 +1,54 @@
-$(function() {
+// // $(function() {
+// //
+// //   alert('The DOM has finished loading')
+// //
+// // });
+//
+//    let firstParagraph = $('#First-p').html()
+// alert(firstParagraph)
+// //
+// // let secondParagraph = $('#Second-p').html()
+// // console.log(secondParagraph)
+//
+// $('.odd').css('border', 'yellow solid 1px')
+//
+// $('li').css('font-size', '50px')
+//
+// $('h1,p,li').css('background-color',  'yellow');
+//
+// let alertHOne = $('h1').html()
+// console.log(alertHOne)
+//
+// // $('*').css('background-color',  'red');
 
-  alert('The DOM has finished loading')
 
-});
+
+// $('h1').click(function($('h1').css('background-color', 'red'){
+
+
+
+// $('h1').click(
+//     function() {
+//        $(this).css('background-color', 'yellow');
+//     };
+
+$('h1').click(
+    function() {
+       $(this).css('color', '#FF0');})
+
+$('p').dblclick(
+    function() {
+       $(this).css('font-size', '18px');})
+
+$('li').hover(
+    function() {
+       $(this).css('background-color', 'red');
+    },
+    function() {
+       $(this).css('background-color', 'black');
+    }
+);
+
+
+
+
