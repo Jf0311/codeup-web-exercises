b0fb49881baba7f803976e2f5796bd65b64cb537
Joseph Fuentes <josephkfuentes@gmail.com>
2023-05-01 20:29:43 -0400

    adding more stuff

--- js/weather_map.js
+++ js/weather_map.js
@@ -23,28 +23,28 @@ $.get("http://api.openweathermap.org/data/2.5/forecast", {
     // use query selector and try to append it to the card
     // This is for the max temp
     document.querySelector("#day1-max").append(weatherForecast.list[0].main.temp_max) ;
-    document.querySelector("#day2-max").append(weatherForecast.list[1].main.temp_max) ;
-    document.querySelector("#day3-max").append(weatherForecast.list[2].main.temp_max) ;
-    document.querySelector("#day4-max").append(weatherForecast.list[3].main.temp_max) ;
-    document.querySelector("#day5-max").append(weatherForecast.list[4].main.temp_max) ;
+    document.querySelector("#day2-max").append(weatherForecast.list[8].main.temp_max) ;
+    document.querySelector("#day3-max").append(weatherForecast.list[16].main.temp_max) ;
+    document.querySelector("#day4-max").append(weatherForecast.list[24].main.temp_max) ;
+    document.querySelector("#day5-max").append(weatherForecast.list[32].main.temp_max) ;
     //this is for min temp
     document.querySelector("#day1-min").append(weatherForecast.list[0].main.temp_min) ;
-    document.querySelector("#day2-min").append(weatherForecast.list[1].main.temp_min) ;
-    document.querySelector("#day3-min").append(weatherForecast.list[2].main.temp_min) ;
-    document.querySelector("#day4-min").append(weatherForecast.list[3].main.temp_min) ;
-    document.querySelector("#day5-min").append(weatherForecast.list[4].main.temp_min) ;
+    document.querySelector("#day2-min").append(weatherForecast.list[8].main.temp_min) ;
+    document.querySelector("#day3-min").append(weatherForecast.list[16].main.temp_min) ;
+    document.querySelector("#day4-min").append(weatherForecast.list[24].main.temp_min) ;
+    document.querySelector("#day5-min").append(weatherForecast.list[32].main.temp_min) ;
     // This is for humidity
     document.querySelector("#day1-humidity").append(weatherForecast.list[0].main.humidity) ;
-    document.querySelector("#day2-humidity").append(weatherForecast.list[1].main.humidity) ;
-    document.querySelector("#day3-humidity").append(weatherForecast.list[2].main.humidity) ;
-    document.querySelector("#day4-humidity").append(weatherForecast.list[3].main.humidity) ;
-    document.querySelector("#day5-humidity").append(weatherForecast.list[4].main.humidity) ;
+    document.querySelector("#day2-humidity").append(weatherForecast.list[8].main.humidity) ;
+    document.querySelector("#day3-humidity").append(weatherForecast.list[16].main.humidity) ;
+    document.querySelector("#day4-humidity").append(weatherForecast.list[24].main.humidity) ;
+    document.querySelector("#day5-humidity").append(weatherForecast.list[32].main.humidity) ;
     //This is for wind speed
     document.querySelector("#day1-conditions").append(weatherForecast.list[0].wind.speed) ;
-    document.querySelector("#day2-conditions").append(weatherForecast.list[1].wind.speed) ;
-    document.querySelector("#day3-conditions").append(weatherForecast.list[2].wind.speed) ;
-    document.querySelector("#day4-conditions").append(weatherForecast.list[3].wind.speed) ;
-    document.querySelector("#day5-conditions").append(weatherForecast.list[4].wind.speed) ;
+    document.querySelector("#day2-conditions").append(weatherForecast.list[8].wind.speed) ;
+    document.querySelector("#day3-conditions").append(weatherForecast.list[16].wind.speed) ;
+    document.querySelector("#day4-conditions").append(weatherForecast.list[24].wind.speed) ;
+    document.querySelector("#day5-conditions").append(weatherForecast.list[32].wind.speed) ;
     //This is for the day of the week
     document.querySelector("#day-1").append(weatherForecast.list[0].dt_txt) ;
     document.querySelector("#day-2").append(weatherForecast.list[8].dt_txt) ;
@@ -54,13 +54,7 @@ $.get("http://api.openweathermap.org/data/2.5/forecast", {
    //
    //  document.querySelector(".logo").append(`${picture}`);
     $('.logo').html(picture)
-    // $('.logo').html(picture1)
-    // $('.logo').html(picture2)
-    // $('.logo').html(picture3)
-    // $('.logo').html(picture4)
 
-
-    //$('#weather').append("<div class='weatherCards'><p class='date'>" + data.list[i].dt_txt.substring(0, 10) + "</p>" +
 });
 
 console.log(MAPBOX_KEY)
